e59f55e9185bf8dc80fe3e85deeb19c214a46d3c
ikade <ikade@KADOEL>
2022-01-16 09:37:00 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace ArrayManual
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lBNama = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lBNama
+            // 
+            this.lBNama.FormattingEnabled = true;
+            this.lBNama.Location = new System.Drawing.Point(12, 28);
+            this.lBNama.Name = "lBNama";
+            this.lBNama.Size = new System.Drawing.Size(252, 134);
+            this.lBNama.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 190);
+            this.Controls.Add(this.lBNama);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lBNama;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayManual
+{
+    public partial class Form1 : Form
+    {
+        private String[] nama = new String[] { "Marhen", "Tono", "Andi", "Riko", "Juni" };
+        //string[] nama = { "Marhen", "Tono", "Andi", "Riko", "Juli" };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            for(int i = 0; i < nama.Length; i++)
+            {
+                lBNama.Items.Add(nama[i]);
+            }
+        }
+    }
+}
